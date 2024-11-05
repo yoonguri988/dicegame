4708774361bf52a49ddeba7cf1e25b986da5d6f5
CHOI YOON JEONG <56820897+yoonguri988@users.noreply.github.com>
2024-11-06 01:15:34 +0900

    Feat: 컴포넌트 (#1)
    주사위 게임 컴포넌트 기초

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const product = "macbook";
-const model = "Air";
-const item = product.toUpperCase() + " " + model;
-const imageUrl =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
-
-function handleClick(e) {
-  alert("곧 도착합니다!");
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <h1>나만의 {item} 주문하기</h1>
-    <img src={imageUrl} alt="제품 사진" />
-    <button onClick={handleClick}>확인</button>
-  </React.StrictMode>
+  <>
+    <App />
+  </>
 );
