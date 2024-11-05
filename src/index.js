badef5b13b46bfa1083e052e452b83c84f140743
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-06 00:51:27 +0900

    Feat: JSX에서 자바스크립 사용하기

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const product = "macbook";
+const model = "Air";
+const item = product.toUpperCase() + " " + model;
+const imageUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
+
+function handleClick(e) {
+  alert("곧 도착합니다!");
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>안녕 리액트</h1>
+    <h1>나만의 {item} 주문하기</h1>
+    <img src={imageUrl} alt="제품 사진" />
+    <button onClick={handleClick}>확인</button>
   </React.StrictMode>
 );
