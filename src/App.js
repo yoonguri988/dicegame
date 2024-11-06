72950c71427251940065daca35080ed951c28e90
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-07 01:32:45 +0900

    Feat: children props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Dice from "./Dice";
+import Button from "./Button";
 
 function App() {
   return (
     <div>
+      <div>
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
+      </div>
       <Dice color="red" num={5} />
     </div>
   );
