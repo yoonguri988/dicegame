74d26aa02a8b806b3dcfa76f97a4aa49f1df49f4
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-07 23:45:40 +0900

    Feat: State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Dice from "./Dice";
 import Button from "./Button";
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+  const handleRollClick = () => {
+    setNum(random(6));
+  };
+  const handleClearClick = () => {
+    setNum(1);
+  };
   return (
     <div>
       <div>
-        <Button>던지기</Button>
-        <Button>처음부터</Button>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="red" num={5} />
+      <Dice color="red" num={num} />
     </div>
   );
 }
