7ab990f317d4170cdf44b496a131a352c9bfff49
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-11-10 00:30:02 +0900

    Feat: className design
    CSS className 적용하기
    classnames 라이브러리 설치

--- src/Button.js
+++ src/Button.js
@@ -1,29 +1,9 @@
-const baseButtonStyle = {
-  padding: "14px 27px",
-  outline: "none",
-  cursor: "pointer",
-  borderRadius: "9999px",
-  fontSize: "17px",
-};
+import "./Button.css";
 
-const blueButtonStyle = {
-  ...baseButtonStyle,
-  border: "solid 1px #7090ff",
-  color: "#7090ff",
-  backgroundColor: "rgba(0, 89, 255, 0.2)",
-};
-
-const redButtonStyle = {
-  ...baseButtonStyle,
-  border: "solid 1px #ff4664",
-  color: "#ff4664",
-  backgroundColor: "rgba(255, 78, 78, 0.2)",
-};
-
-function Button({ color, children, onClick }) {
-  const style = color === "red" ? redButtonStyle : blueButtonStyle;
+function Button({ color = "blue", children, onClick, className = "" }) {
+  const classNames = `Button ${color} ${className}`;
   return (
-    <button style={style} onClick={onClick}>
+    <button className={classNames} onClick={onClick}>
       {children}
     </button>
   );
